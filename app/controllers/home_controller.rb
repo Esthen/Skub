ece8629ab409d29039d01b4e38cb10901206e3c4
Esthen <vestaroid@gmail.com>
2015-08-15 23:20:24 +0200

    code can't be used twice

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,13 @@ class HomeController < ApplicationController
   end
 
   def session_upgrade
-    if (params.has_key?(:key) && Code.where("key = '#{params[:key]}'").empty? == false)
+    if (params.has_key?(:key) &&
+       Code.where("key = '#{params[:key]}'").empty? == false &&
+       Code.where("key = '#{params[:key]}'").first[:isactive] == true)
       session[:key] = 413
+      test = Code.where("key = '#{params[:key]}'").first
+      test[:isactive] = false
+      test.save
       redirect_to new_registration_path(:account)
     else
       session[:key] = 612
