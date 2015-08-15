9f1bed078227009f4d5d7242ee169a66bc3d1868
Esthen <vestaroid@gmail.com>
2015-08-15 22:00:01 +0200

    prepare overide of devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :accounts
+  devise_for :accounts, controllers: { sessions: "accounts/sessions" }
   # Home page where you can enter closed beta code. Only page accessible in 0.0001v
   root 'home#code' #get 'home/code'
   get 'up' => 'home#session_upgrade'
